247fc14ffaa9a7b8a7e2066c27924231436f4a81
ping <ping@users.noreply.github.com>
2022-08-16 20:05:33 +0800

    [custom] Add more examples of recipe customisation

--- _recipes_custom.py
+++ _recipes_custom.py
@@ -1,14 +1,28 @@
 from typing import List
 
-from _recipe_utils import Recipe
+from _recipe_utils import Recipe, CoverOptions, onlyon_weekdays
 
 # Define your custom recipes list here
 recipes: List[Recipe] = [
+    # Custom recipe example (recipes_custom/example.recipe.py)
     Recipe(
         recipe="example",
-        slug="example",
+        slug="example-01",
         src_ext="epub",
         category="example",
         target_ext=["pdf"],
+        cover_options=CoverOptions(
+            text_colour="white",
+            background_colour="black",
+        ),  # generate black cover with white text
+    ),
+    # Builtin Calibre recipe example
+    Recipe(
+        recipe="Macrobusiness",
+        name="Macrobusiness",
+        slug="macrobusiness",
+        src_ext="epub",
+        category="example",
+        enable_on=onlyon_weekdays([1, 3, 5], 10),  # tues, thurs, sat
     ),
 ]
