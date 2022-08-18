4532d11cd922cc4a245ca0b5c4c7b97b78e53c79
ping <ping@users.noreply.github.com>
2022-08-18 12:58:55 +0800

    [custom] Demo categories

--- _recipes_custom.py
+++ _recipes_custom.py
@@ -3,7 +3,7 @@ from typing import List
 from _recipe_utils import Recipe, CoverOptions, onlyon_weekdays
 
 # Define the categories display order, optional
-categories_sort: List[str] = ["example", "magazines"]
+categories_sort: List[str] = ["Examples", "Example Magazines"]
 
 # Define your custom recipes list here
 # Example: https://github.com/ping/newsrack-fork-test/blob/custom/_recipes_custom.py
@@ -14,7 +14,7 @@ recipes: List[Recipe] = [
         recipe="example",
         slug="example-01",
         src_ext="epub",
-        category="example",
+        category="Examples",
         target_ext=["pdf"],
         cover_options=CoverOptions(
             text_colour="white",
@@ -27,7 +27,7 @@ recipes: List[Recipe] = [
         name="Macrobusiness",
         slug="macrobusiness",
         src_ext="epub",
-        category="example",
+        category="Examples",
         enable_on=onlyon_weekdays([1, 3, 5], 10),  # tues, thurs, sat
     ),
     # newsrack builtin recipe
@@ -36,6 +36,6 @@ recipes: List[Recipe] = [
         slug="vox",
         src_ext="mobi",
         target_ext=["epub"],
-        category="magazines",
+        category="Example Magazines",
     ),
 ]
